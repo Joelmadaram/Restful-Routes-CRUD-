0fe5387c63457aa3aeaf60e5fe2e7ff7e2692191
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-06-19 11:45:07 -0500

    Creating Restful Routes by adding comments

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+const comments = [
+    {
+        username: 'mike',
+        comment: 'I love tacos'
+    },
+    {
+        username: 'bob',
+        comment: 'I love burritos'
+    },
+    {
+        username: 'joe',
+        comment: 'I love sushi'
+    },
+    {
+        username: 'james',
+        comment: 'I love biryani'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+})
 
 app.get('/tacos', (req, res) => {
     res.send('Get /tacos response')
