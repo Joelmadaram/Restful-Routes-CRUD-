6e7dba06c1c92304a283c6ea2a3bb3096afb3eee
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-06-19 18:03:41 -0500

    using uuid package to generate unique identifiers

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
+uuid(); 
+
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
@@ -9,18 +13,22 @@ app.set('view engine', 'ejs')
 
 const comments = [
     {
+        id: uuid(),
         username: 'mike',
         comment: 'I love tacos'
     },
     {
+        id:uuid(),
         username: 'bob',
         comment: 'I love burritos'
     },
     {
+        id: uuid(),
         username: 'joe',
         comment: 'I love sushi'
     },
     {
+        id: uuid(),
         username: 'james',
         comment: 'I love biryani'
     }
@@ -37,9 +45,15 @@ app.get('/comments/new', (req, res) => {
 app.post('/comments', (req, res) => {
     console.log(req.body);
     const {username, comment} = req.body;
-    comments.push({username, comment })
+    comments.push({username, comment, id: uuid() })
     res.redirect('/comments')
 })
+
+app.get('/comments/id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment })
+    })
 
 app.get('/tacos', (req, res) => {
     res.send('Get /tacos response')
