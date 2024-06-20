06e276cac3efbdbfcf8acb8059c4e46b17dd632f
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-06-20 16:08:44 -0500

    updating comments using crud methods

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { v4: uuid } = require('uuid');
-uuid(); 
-
-}
+const methodOverride = require('method-override');
+const { v4: uuid } = require('uuid')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
 
 const comments = [
     {
@@ -46,14 +46,28 @@ app.post('/comments', (req, res) => {
     console.log(req.body);
     const {username, comment} = req.body;
     comments.push({username, comment, id: uuid() })
-    res.redirect('/comments')
+    res.redirect('/comments');
 })
 
-app.get('/comments/id', (req, res) => {
+app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id === id);
     res.render('comments/show', { comment })
-    })
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment })
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const foundComment = comments.find(c => c.id === id);
+    const { newCommentText } = req.body;
+    foundComment.comment = newCommentText;
+    res.redirect('/comments') 
+})
 
 app.get('/tacos', (req, res) => {
     res.send('Get /tacos response')
